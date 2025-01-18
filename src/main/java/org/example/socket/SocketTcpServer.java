b8867661683c532e19602e3e05273011d587b482
fmandia <fmandia1201@gmail.com>
2025-01-18 21:08:50 +0100

    HandlerClient.java SocketTcpServer.java

---
+++ src/main/java/org/example/socket/SocketTcpServer.java
@@ -0,0 +1,26 @@
+package org.example.socket;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SocketTcpServer {
+
+    private ServerSocket serverSocket;
+    private Socket socket;
+    private InputStream is;
+    private OutputStream os;
+    private BufferedReader br;
+    private PrintWriter pw;
+
+    public SocketTcpServer() {
+
+    }
+
+    public void start() {
+
+    }
+}
