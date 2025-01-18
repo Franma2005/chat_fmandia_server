7c07e739be5ed03f34477f3556df31fd1d6a6bcd
fmandia <fmandia1201@gmail.com>
2025-01-18 22:00:07 +0100

    Singleton implemented

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,10 @@
 package org.example;
 
+import org.example.services.HandlerClient;
 import org.example.socket.SocketTcpServer;
 
 public class Main {
     public static void main(String[] args) {
-        SocketTcpServer socketTcpServer = new SocketTcpServer();
+        HandlerClient handlerClient = HandlerClient.getInstance();
     }
 }
--- src/main/java/org/example/services/HandlerClient.java
+++ src/main/java/org/example/services/HandlerClient.java
@@ -2,5 +2,14 @@ package org.example.services;
 
 public class HandlerClient {
 
+    private static HandlerClient instance;
 
+    private HandlerClient() {
+    }
+
+    public static HandlerClient getInstance() {
+        if (instance == null)
+            instance = new HandlerClient();
+        return instance;
+    }
 }
