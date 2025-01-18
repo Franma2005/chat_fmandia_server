1ed6aaf5f987987e273423656b0aa214758691c0
fmandia <fmandia1201@gmail.com>
2025-01-18 21:18:01 +0100

    SocketTcpServer.java

--- src/main/java/org/example/socket/SocketTcpServer.java
+++ src/main/java/org/example/socket/SocketTcpServer.java
@@ -1,9 +1,6 @@
 package org.example.socket;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -16,11 +13,37 @@ public class SocketTcpServer {
     private BufferedReader br;
     private PrintWriter pw;
 
-    public SocketTcpServer() {
-
+    public SocketTcpServer(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
     }
 
-    public void start() {
+    public void start() throws IOException{
+        System.out.println("(Server) Opening the channels of communication");
+        socket = serverSocket.accept();
+        is = socket.getInputStream();
+        os = socket.getOutputStream();
+        System.out.println("(Client) The channels of communication have been opened");
+    }
 
+    public void startTextChannels() {
+        System.out.println("(Server) Opening the text channels of communication");
+        br = new BufferedReader(new InputStreamReader(is));
+        pw = new PrintWriter(os, true);
+        System.out.println("(Server) The text channels of communication have been openned");
+    }
+
+    public void stop() throws IOException {
+        System.out.println("(Server) Closing the channels of communication");
+        os.close();
+        is.close();
+        socket.close();
+        System.out.println("(Server) The channels of communication have been opened");
+    }
+
+    public void stopTextChannels() throws IOException {
+        System.out.println("(Server) Closing the text channels of communicantion");
+        br.close();
+        pw.close();
+        System.out.println("(Server) The text channels of communication have been closed");
     }
 }
